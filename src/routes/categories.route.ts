af7c186045ce4b964a476e2439cbf6d15ee02280
Marcelo Ferreira <mah.tf@hotmail.com>
2021-10-25 09:05:24 -0300

    Created ListCategories UseCase

--- src/routes/categories.route.ts
+++ src/routes/categories.route.ts
@@ -1,19 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { createCategoryController } from "../modules/cars/useCases/createCategory/index";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const allCategories = categoriesRepository.list();
-
-  return response.json(allCategories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
