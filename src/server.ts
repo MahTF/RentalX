ef5eb823f2434ad147f70259c5177dce3bf2fbaa
Marcelo Ferreira <mah.tf@hotmail.com>
2021-11-04 14:52:19 -0300

    Created AppError

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import swaggerUi from "swagger-ui-express";
+import "express-async-errors";
 import "reflect-metadata";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerConfigs from "./swagger.json";
 import "./database";
@@ -15,5 +17,20 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfigs));
 
 app.use("/", router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
